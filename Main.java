3f1c8a00941d6aedb2ecd6b16ddf3bbb1334ce00
MRr-Adi <120578864+MRr-Adi@users.noreply.github.com>
2024-01-02 22:50:57 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        // Here we are going to find Area of a Circle
+        float rad = sc.nextInt();
+        float area = 3.14f * rad * rad ;
+
+
+        System.out.println(area);
+    }
+}
