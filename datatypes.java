c2e1f7d608fd8e98aee2c9b7a7358a76aad2a108
MRr-Adi <120578864+MRr-Adi@users.noreply.github.com>
2025-01-02 22:29:42 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,44 @@
+public class datatypes {
+    public static void main(String args[]){
+
+        //Primitive Datatypes
+        //Numeric Datatypes
+        //Integers 
+
+        int i = 5;
+        System.out.println("integer:" + i);
+
+        short s = 6;
+        System.out.println("short:" + s);
+
+        long l = 7;
+        System.out.println("long:" + l);
+        
+        byte b = 8;
+        System.out.println("byte:" + b);
+
+
+        //Numeric Datatypes
+        //Decimals
+
+        // for float use 'f' as suffix as standard
+        float f = 9.5f;
+        System.out.println("float:" + f);
+
+        double d = 10.5;
+        System.out.println("double:" + d);
+
+        //Non-Numeric Datatypes
+        //Characters and True or False 
+
+        char c = 'a';
+        System.out.println("char:" + c);
+
+        boolean t = false;
+        System.out.println("boolean:" + t);
+
+
+
+    }
+    
+}
