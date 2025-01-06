33df6258cb7c9025c14ea2119fecb343afc150ac
MRr-Adi <120578864+MRr-Adi@users.noreply.github.com>
2025-01-06 21:34:01 +0530

    Add files via upload

---
+++ do_while.java
@@ -0,0 +1,12 @@
+public class do_while{
+    public static void main (String[] args){
+        int a = 1;
+        do{
+            
+            System.out.println("HW");
+            a++;
+        }while(a>=10);
+
+        
+    }
+} 
