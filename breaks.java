a2ac057937fdcec6d42d7461ea27f4053a951591
MRr-Adi <120578864+MRr-Adi@users.noreply.github.com>
2025-01-09 23:04:36 +0530

    Add files via upload

---
+++ breaks.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class breaks {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        
+
+        do{
+            System.out.println("enter your number");
+            int a = sc.nextInt();   
+           
+             
+            if(a%10==0){
+                System.out.println(a);
+                
+                break;
+                
+            }
+    }while(true);
+    
+}
+    
+    
+}
