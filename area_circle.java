3017e750d51f9e1d9509b9fa6f58872be9bc0a0c
MRr-Adi <120578864+MRr-Adi@users.noreply.github.com>
2025-01-11 23:05:11 +0530

    Add files via upload

---
+++ area_circle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class area_circle {
+    public static void main(String args[]){
+         Scanner sc = new Scanner(System.in) {
+            int radius = sc.nextInt();
+            
+            
+            float area = 3.14f*radius*radius;
+
+            System.out.println("area of a circle=" + area);
+        }
+
+
+    }
+    
+    
+}
