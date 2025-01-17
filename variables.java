ee3e31e49fe5ce45a7b35b33ad08c096940f8c2f
MRr-Adi <120578864+MRr-Adi@users.noreply.github.com>
2025-01-17 23:51:24 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,17 @@
+public class variables {
+    public static void main(String arg[]){
+        
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a);
+
+        String name = "Bruce Wayne";
+        System.out.println(name);
+
+        a=b;
+        System.out.println(a);
+
+
+    }
+}
